e9fb25498c594a22100c0ca7bb7450f9bc183ca9
Schmiddiii <schmidhuberj41@gmail.com>
2019-03-14 19:18:04 +0100

    Added Parameters usable in the command-line

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"log"
+	"math"
 	"os"
 	"sync"
 )
 
-var width = 2048
-var height = 2048
-
-var xFrom, xTo, yFrom, yTo float64 = -2, 1, -1.5, 1.5
-
-var maxIter uint8 = 30
+var xFrom, xDelta, yFrom, yDelta float64 //= -2, 3, -1.5, 3
+var width, height int
 
 func main() {
-	fmt.Println("Starting")
+
+	flag.Float64Var(&xFrom, "x", -2, "Starting x-value")
+	flag.Float64Var(&yFrom, "y", -1.5, "Starting y-value")
+
+	flag.Float64Var(&xDelta, "dx", 3, "Delta x-value")
+	flag.Float64Var(&yDelta, "dy", 3, "Delta y-value")
+
+	flag.IntVar(&width, "w", 2048, "Width of Image")
+	flag.IntVar(&height, "h", 2048, "Delta y-value")
+
+	var maxIter64 uint64
+	flag.Uint64Var(&maxIter64, "i", 30, "Maximal Iterations")
+	maxIter := uint8(maxIter64)
+
+	flag.Parse()
+
+	fmt.Printf("Starting with x: %f, y: %f, dx: %f, dy: %f, i: %d\n", xFrom, xDelta, yFrom, yDelta, maxIter)
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{width, height}
@@ -32,13 +46,15 @@ func main() {
 		go func(x int) {
 			for y := 0; y < height; y++ {
 				col := color.RGBA{}
-				mandelX := (float64(x)/float64(width))*(xTo-xFrom) + xFrom
-				mandelY := (float64(y)/float64(height))*(yTo-yFrom) + yFrom
-				iterationen := Mandelbrot(mandelX, mandelY, mandelX, mandelY, int(maxIter))
+				mandelX := (float64(x)/float64(width))*xDelta + xFrom
+				mandelY := (float64(y)/float64(height))*yDelta + yFrom
+				iterationen := mandelbrot(mandelX, mandelY, mandelX, mandelY, int(maxIter))
 
-				col.R = 255 / maxIter * iterationen
-				col.G = 255 / maxIter * iterationen
-				col.B = 255 / maxIter * iterationen
+				if iterationen == maxIter {
+
+				} else {
+					col = hsv2rgb(1080*float64(iterationen)/float64(maxIter), 1, 1, 255)
+				}
 
 				col.A = 255 / maxIter * iterationen
 
@@ -61,7 +77,7 @@ func main() {
 }
 
 //Mandelbrot function from Wikipedia
-func Mandelbrot(x, y, xAdd, yAdd float64, maxIter int) uint8 {
+func mandelbrot(x, y, xAdd, yAdd float64, maxIter int) uint8 {
 	maxBetrag2 := 4.0
 	remainIter := maxIter
 	xx := x * x
@@ -79,4 +95,30 @@ func Mandelbrot(x, y, xAdd, yAdd float64, maxIter int) uint8 {
 		betrag2 = xx + yy
 	}
 	return uint8(maxIter - remainIter)
+}
+
+func hsv2rgb(h, s, v, a float64) color.RGBA {
+	h = modFloat(h, 360)
+	c := v * s
+	x := c * (1 - math.Abs(float64(modFloat(h/60, 2.0)-1)))
+	m := v - c
+	if h < 60 {
+		return color.RGBA{R: uint8((c + m) * 255), G: uint8((x + m) * 255), B: uint8(((0 + m) * 255)), A: uint8(a)}
+	} else if h < 120 {
+		return color.RGBA{R: uint8((x + m) * 255), G: uint8((c + m) * 255), B: uint8(((0 + m) * 255)), A: uint8(a)}
+	} else if h < 180 {
+		return color.RGBA{R: uint8((0 + m) * 255), G: uint8((c + m) * 255), B: uint8(((x + m) * 255)), A: uint8(a)}
+	} else if h < 240 {
+		return color.RGBA{R: uint8((0 + m) * 255), G: uint8((x + m) * 255), B: uint8(((c + m) * 255)), A: uint8(a)}
+	} else if h < 300 {
+		return color.RGBA{R: uint8((x + m) * 255), G: uint8((0 + m) * 255), B: uint8(((c + m) * 255)), A: uint8(a)}
+	} else {
+		return color.RGBA{R: uint8((x + m) * 255), G: uint8((0 + m) * 255), B: uint8(((c + m) * 255)), A: uint8(a)}
+	}
+
+}
+
+func modFloat(m, f float64) float64 {
+	num := int(m / f)
+	return m - float64(num)*f
 }
